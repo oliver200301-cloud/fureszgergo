3d6f0f7d418465bd766d4e916844433b8a30ad55
koderich <richi@elife.hu>
2026-07-22 17:13:09 +0200

    Test

---
+++ .wrangler/tmp/bundle-QsubB2/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "C:\\Users\\richi\\AppData\\Local\\pnpm\\store\\v11\\links\\@\\wrangler\\4.110.0\\8ef2581937d995d66e94a01c10b79fecb7e7abdd44ef2330dd454860de5025d6\\node_modules\\wrangler\\templates\\pages-shim.ts";
+				import * as __MIDDLEWARE_0__ from "C:\\Users\\richi\\AppData\\Local\\pnpm\\store\\v11\\links\\@\\wrangler\\4.110.0\\8ef2581937d995d66e94a01c10b79fecb7e7abdd44ef2330dd454860de5025d6\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "C:\\Users\\richi\\AppData\\Local\\pnpm\\store\\v11\\links\\@\\wrangler\\4.110.0\\8ef2581937d995d66e94a01c10b79fecb7e7abdd44ef2330dd454860de5025d6\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "C:\\Users\\richi\\AppData\\Local\\pnpm\\store\\v11\\links\\@\\wrangler\\4.110.0\\8ef2581937d995d66e94a01c10b79fecb7e7abdd44ef2330dd454860de5025d6\\node_modules\\wrangler\\templates\\pages-shim.ts";
+				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
